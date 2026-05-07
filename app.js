0f5300d6fa61889954b5168d982466a6d5bbb5a9
bryantwhitsett <bryantwhitsett71@gmail.com>
2026-05-07 07:03:51 -0400

    JS Variable

--- app.js
+++ app.js
@@ -4,7 +4,40 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Bryant";
+let lastName;
+var age;
 
 // Exercise 2
+lastName = "Whitsett";
+age = 32;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first used in the year ${createdYear}`)
+// C# String Interpolation - Console.WriteLine($"{} ---- {} ---.");
+
+// Exercise 4
+let product = "Rain Coat"
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(
+  ` I just bought a ${product} for $${price}. I got it on sale for
+  ${salePercentage} percent off and they almost had ${stock} in stock so I had 
+  to get it. Only thing is it was a ${selectedSize} and I'm a large so I
+  hope it fits but if not I'll give it to my little cousin no biggy. `)
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
